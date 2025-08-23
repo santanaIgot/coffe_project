89a362fe4696be5f8db96ab6562698cabdd88ddf
santanaIgot <santanaigoor01@gmail.com>
2025-08-23 16:37:01 -0300

    [feature] - implemented heading

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,20 +1,45 @@
-
-
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import CoffeImage from "../../assets/Coffe.svg";
+import { Heading, HeroContent, HeroSection, Info } from "./styles";
 
 export function Home() {
-    return(
-        <div>
+  return (
+    <div>
+      <HeroSection>
+        <HeroContent>
+          <div>
+            <Heading>
+              <h1>
+                Encontre o café perfeito para <br /> qualquer hora do dia{" "}
+              </h1>
+              <span>
+                Com o coffe delivery você recebe seu café onde estiver
+              </span>
+            </Heading>
 
-            <div>
-                <h1>Encontre o café perfeito para qualquer hora do dia </h1>
-                <h3>Com o coffe delivery você recebe seu café onde estiver</h3>
-            </div>
-          
+            <Info>
+              <div>
+                <ShoppingCart />
+                <span>Compra simples e segura</span>
+              </div>
+              <div>
+                <Package/>
+                <span>Embalagem mantém o café intacto</span>
+              </div>
+              <div>
+                <Timer/>
+                <span>Entrega rápida e rastreada</span>
+              </div>
+              <div>
+                <Coffee/>
+                <span>O café chega fresquinho até você</span>
+              </div>
+            </Info>
 
-            <p>Compra simples e segura</p>
-            <p>Entrega rápida e rastreada</p>
-            <p>Embalagem mantém o café intacto</p>
-            <p>O café chega fresquinho até você</p>
-        </div>
-    )
+            <img src={CoffeImage} id="hero-bg" />
+          </div>
+        </HeroContent>
+      </HeroSection>
+    </div>
+  );
 }
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,4 +1,69 @@
 import {styled} from  "styled-components"
+import { mixins } from "../../styles/mixins"
 
 
 
+export const HeroSection = styled.section`
+    position: relative;
+
+    img#hero-bg{
+        position: absolute;
+        top: 100px;
+        left: 0;
+        max-height:350px;
+        width: 135vw;
+        object-fit: contain;
+    }
+`
+
+export const Heading = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    > h1{
+        ${mixins.fonts.titleXL}
+        color: ${({theme}) => theme.colors["base-title"] };
+    }
+
+    > span{
+        ${mixins.fonts.titleL}
+        color: ${({theme}) => theme.colors["base-subtitle"]};
+    }
+
+`
+
+export const HeroContent = styled.div`
+    max-width: 1160px;
+    padding: 92px 20px;
+    margin: 0 auto;
+    display: flex;
+    gap: 56px;
+
+    > div{
+        display: flex;
+        flex-direction: column;
+        gap: 66px;
+    }
+    
+`
+
+
+export const Info = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-row-gap: 12px;
+
+    > div{
+        display:flex;
+        align-items: center;
+        gap: 12px;
+    }
+
+    svg{
+        padding: 1px;
+        border-radius: 999px;
+    }
+
+    
+`
